5d376f45f3c132c51fb93acbdba0bac225d881c9
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2024-06-26 20:42:03 +0300

    init commit

---
+++ src/main/kotlin/com/demo/leonovets/springrabbitmqdemo/SpringRabbitMqDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.leonovets.springrabbitmqdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRabbitMqDemoApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringRabbitMqDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/leonovets/springrabbitmqdemo/SpringRabbitMqDemoApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.demo.leonovets.springrabbitmqdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRabbitMqDemoApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+}
